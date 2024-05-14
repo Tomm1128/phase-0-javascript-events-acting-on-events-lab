ec381f21c908300dabb03c1f61e57fc3cc764e30
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-05-14 11:55:40 -0400

    Target dodger and make it move left

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Your code here
+
+
+const dodger = document.getElementById("dodger");
+
+const moveDodgerLeft = () => {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  } 
+});
+
+
